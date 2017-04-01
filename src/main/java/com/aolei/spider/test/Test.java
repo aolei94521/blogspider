ca3939a3062616e54dbe66f671beb58d7985b29e
aolei <807648567@qq.com>
2017-04-01 21:33:18 +0800

    修改了一些配置文件

--- src/main/java/com/aolei/spider/test/Test.java
+++ src/main/java/com/aolei/spider/test/Test.java
@@ -1,23 +1,29 @@
 package com.aolei.spider.test;
 
-import com.aolei.spider.entity.InformationEntity;
 import com.aolei.spider.service.CSDNInformationService;
-import com.aolei.spider.service.SpringContextHolder;
-
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Administrator on 2017/3/29.
  */
 public class Test {
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
     public static void main(String args[]){
-        CSDNInformationService csdnInformationService = SpringContextHolder.getBean(CSDNInformationService.class);
+        /*CSDNInformationService csdnInformationService = SpringContextHolder.getBean(CSDNInformationService.class);
         if (csdnInformationService != null){
             List<InformationEntity> list =  csdnInformationService.getAllInformation();
             if (list!= null && !list.isEmpty()){
                 System.out.println("test");
             }
+        }*/
+        AbstractApplicationContext app = new ClassPathXmlApplicationContext(new String[]{"classpath:\\spring-mvc.xml"});
+        CSDNInformationService csdnInformationService = (CSDNInformationService)app.getBean("CSDNInformationServiceImpl.class");
+        if (csdnInformationService.getAllInformation() == null || csdnInformationService.getAllInformation().isEmpty()){
+            System.out.println("sss");
         }
-
+        app.close();
     }
 }
